6c4705705f91c44b17b21a8a82c1ca65ace0fc14
Kevin Chen <kevin.chen@veeva.com>
2015-03-26 13:17:45 -0400

    Did some initial work on the splash screen

--- both/controllers/app.js
+++ both/controllers/app.js
@@ -1,3 +1,11 @@
 AppController = RouteController.extend({
     layoutTemplate: "app"
 });
+AppController.events({
+    "click #sign-in": function(ev, template) {
+        var $el = $(ev.target);
+        var $loginButton = $("#login-buttons .login-button");
+
+        $loginButton.trigger("click");
+    }
+});
--- both/controllers/home.js
+++ both/controllers/home.js
@@ -1,11 +1,3 @@
 HomeController = AppController.extend({
     data: {}
 });
-HomeController.events({
-    "click #sign-in": function(ev, template) {
-        var $el = $(ev.target);
-        var $loginButton = $("#login-buttons .login-button");
-
-        $loginButton.trigger("click");
-    }
-});
