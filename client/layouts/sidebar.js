1251355c3a91c1d712f9cdd85a16e54222f31710
Kevin Chen <kevin.chen@veeva.com>
2015-03-27 21:11:42 -0400

    Listed eligible pages on navigation bar

--- client/layouts/sidebar.js
+++ client/layouts/sidebar.js
@@ -14,5 +14,14 @@ Template.sidebar.events({
  * TEMPLATE HELPERS
  *========================================================================*/
 Template.sidebar.helpers({
-    // ...
+    navigation: function() {
+        var items = [
+            {icon: "fa-user", label: "Dashboard"},
+            {icon: "fa-th-large", label: "Gallery"},
+            {icon: "fa-star", label: "Featured"},
+            {icon: "fa-users", label: "People"},
+            {icon: "fa-wrench", label: "Admin"}
+        ];
+        return items;
+    }
 });
