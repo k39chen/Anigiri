28d0c4d25c346d0a3f84d053b49f9b2ac9d57c9a
Kevin Chen <kevin.chen@veeva.com>
2015-04-26 13:23:02 -0400

    Registered for TVDB API and added registration key

--- server/methods/_config.js
+++ server/methods/_config.js
@@ -1,7 +1,8 @@
 API_CONFIG = {
-    ANN_API            : 'https://animenewsnetwork.p.mashape.com/api.xml',
-    ANN_REPORTS        : 'https://animenewsnetwork.p.mashape.com/reports.xml',
-    HUMMINGBIRD_API    : 'https://hummingbirdv1.p.mashape.com/anime/',
-    HUMMINGBIRD_SEARCH : 'http://hummingbird.me/search?query={{hbAnimeId}}&type=anime',
-    MASHAPE_KEY        : 'pT8ejx9ujTSBpdtA3Dz3BT9KdIZn77VK'
+    ANN_API            : "https://animenewsnetwork.p.mashape.com/api.xml",
+    ANN_REPORTS        : "https://animenewsnetwork.p.mashape.com/reports.xml",
+    HUMMINGBIRD_API    : "https://hummingbirdv1.p.mashape.com/anime/",
+    HUMMINGBIRD_SEARCH : "http://hummingbird.me/search?query={{hbAnimeId}}&type=anime",
+    MASHAPE_KEY        : "pT8ejx9ujTSBpdtA3Dz3BT9KdIZn77VK",
+    TVDB_KEY           : "B8E800CFF0D85989"
 };
