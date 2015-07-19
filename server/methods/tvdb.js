4a26f71474c5a3ea254dc55769ef728841987f15
Kevin Chen <kevin.chen@veeva.com>
2015-07-19 17:57:40 -0400

    Formatted TVDB API results

--- server/methods/tvdb.js
+++ server/methods/tvdb.js
@@ -66,11 +66,13 @@ TVDB = _.extend(TVDB, {
         console.log("Received a response, converting XML to JSON format...");
         var json = xml2js.parseStringSync(response);
         var tvdb = json.Data;
-        var result = {};
-
-        // TODO: kchen - Do this
-        result = tvdb;
-
+        var result = {
+            animes: []
+        };
+        // format each type of anime entry
+        result.animes = _.map(tvdb.Series, function(anime) {
+            return TVDB.formatEntry(anime);
+        });
         return result;
     },
     /**
@@ -82,6 +84,20 @@ TVDB = _.extend(TVDB, {
      * @return {Object} The formatted entry data.
      */
     formatEntry: function(entry) {
-        return entry;
+        var picture = _.first(entry.banner) || null;
+
+        if (!_.isNull(picture)) {
+            picture = "http://thetvdb.com/banners/"+picture;
+        }
+        return {
+            "id": _.first(entry.id) || null,
+            "imdb_id": _.first(entry.IMDB_ID) || null,
+            "language": _.first(entry.language) || null,
+            "title": _.first(entry.SeriesName) || null,
+            "picture": picture,
+            "synopsis": _.first(entry.Overview) || null,
+            "startDate": _.first(entry.FirstAired) || null,
+            "network": _.first(entry.Network) || null
+        };
     }
 });
